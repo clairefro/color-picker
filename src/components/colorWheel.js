ddb22073ce50dee236cdc1cc2a583b934eb97298
claire froelich <claire.froelich@gmail.com>
2020-06-08 15:03:19 -0400

    add sound

--- src/components/colorWheel.js
+++ src/components/colorWheel.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import ReactColorWheel from 'react-touch-color-wheel';
+import UIfx from 'uifx'
+
+// sound
+import sound from '../assets/tick.mp3';
 
 const ColorWheel = ( { getColor } ) => {
+  const [tick, setTick]= useState({play: ()=>console.log("can't play yet")})
   const [color, setColor] = useState('')
 
   useEffect(()=> {
     getColor(color)
   },[color])
 
+  useEffect(()=> {
+    setTick(new UIfx(sound, {volume: 0.4, throttleMs: 80}))
+  },[])
+
+  const handleChange = (c) => {
+    setColor(c);
+    tick.play();
+  }
   return (
     <div className="color-wheel">
       <ReactColorWheel
         radius={100}
         // this will be called once the user has indicated they want a color
-        onPreview={ (color) => setColor(color)}
+        onPreview={ (color) => handleChange(color)}
       />
     </div>
   );
