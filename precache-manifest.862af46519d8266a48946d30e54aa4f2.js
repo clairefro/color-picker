4b1108b5ee866e332eaa544b3a04ff91c97a8ae3
claire froelich <claire.froelich@gmail.com>
2020-06-08 12:23:11 -0400

    Updates

---
+++ precache-manifest.862af46519d8266a48946d30e54aa4f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261c314a87dff5260ace3f01f0e21ae5",
+    "url": "/color-picker/index.html"
+  },
+  {
+    "revision": "748d1601ec8243df7e84",
+    "url": "/color-picker/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "c7d15a704a284c7033d0",
+    "url": "/color-picker/static/js/2.9a365603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-picker/static/js/2.9a365603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748d1601ec8243df7e84",
+    "url": "/color-picker/static/js/main.d2971c26.chunk.js"
+  },
+  {
+    "revision": "38793e042a385eeb6c66",
+    "url": "/color-picker/static/js/runtime-main.67166745.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/color-picker/static/media/mintbean.cf18b629.png"
+  }
+]);
